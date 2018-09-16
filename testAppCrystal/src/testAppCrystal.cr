aa68210dd57ee410610aeeedaa07804e69016f7a
NGUYEN KY-ANH <ky-anh.nguyen@etu.unistra.fr>
2018-09-16 17:27:08 +0200

    auth successful

--- testAppCrystal/src/testAppCrystal.cr
+++ testAppCrystal/src/testAppCrystal.cr
@@ -1,15 +1,38 @@
 require "kemal"
+require "http/client"
+require "json"
+
+class Response
+  JSON.mapping(
+    status: String,
+    token: String?,
+  )
+end
 
 get "/" do
   "Hello World!"
 end
 
-post "/connect" do
-  File.read "src/connect.html"
+post "/connect" do |env|
+  data = {
+    "login":    env.params.body["email"]?,
+    "password": env.params.body["password"]?,
+  }.to_json
+  response = HTTP::Client.post("http://0.0.0.0:3000/tokens", headers: HTTP::Headers{"Content-Type" => "application/json"}, body: data)
+  res = Response.from_json(response.body)
+  puts res.status
+
+  if res.status = "success"
+    env.redirect "/connectSuccess" # redirect to /login page
+  end
 end
 
 get "/connect" do
   File.read "src/connect.html"
+end
+
+get "/connectSuccess" do
+  "Connection successful"
 end
 
 Kemal.run do |config|
